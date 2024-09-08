5407dab3ca210add454eb53eb56ec436960a8ac9
Mirek Malinowski <mr.miroslaw.malinowski@gmail.com>
2024-09-08 13:15:04 +0100

    Removed header check

--- not1mm/lib/ft8_watcher.py
+++ not1mm/lib/ft8_watcher.py
@@ -72,44 +72,44 @@ class FT8Watcher:
         logger.debug("PRINT DATAGRAM: {0}".format(datagram))
 #        if datagram[0:4] != b"\xad\xbc\xcb\xda":
 #            return  # bail if no wsjt-x magic number
-        version = self.getuint(datagram[4:8])
-        packettype = self.getuint(datagram[8:12])
-        uniquesize = self.getint(datagram[12:16])
-        unique = datagram[16 : 16 + uniquesize].decode()
-        payload = datagram[16 + uniquesize :]
-
-        if packettype == 0:  # Heartbeat
-            hbmaxschema = self.getuint(payload[0:4])
-            hbversion_len = self.getint(payload[4:8])
-            hbversion = payload[8 : 8 + hbversion_len].decode()
-            print(
-                f"heartbeat: sv:{version} p:{packettype} "
-                f"u:{unique}: ms:{hbmaxschema} av:{hbversion}"
-            )
-            return
-
-        if packettype == 1:  # Status
-            ...
-            [dialfreq] = struct.unpack(">Q", payload[0:8])
-            modelen = self.getint(payload[8:12])
-            mode = payload[12 : 12 + modelen].decode()
-            payload = payload[12 + modelen :]
-            dxcalllen = self.getint(payload[0:4])
-            dxcall = payload[4 : 4 + dxcalllen].decode()
-            print(
-                f"Status: sv:{version} p:{packettype} u:{unique} df:{dialfreq} m:{mode} dxc:{dxcall}"
-            )
-
-            # if f"{dxcall}{self.band}{self.mode}" in self.dupdict:
-            #     self.ft8dupe = f"{dxcall} {self.band}M {self.mode} FT8 Dupe!"
-            return
-
-        if packettype == 2:  # Decode commented out because we really don't care
-            return
-
-        if packettype != 12:
-            return  # bail if not logged ADIF
-        # if log packet it will contain this nugget.
+#        version = self.getuint(datagram[4:8])
+#        packettype = self.getuint(datagram[8:12])
+#        uniquesize = self.getint(datagram[12:16])
+#        unique = datagram[16 : 16 + uniquesize].decode()
+#        payload = datagram[16 + uniquesize :]
+#
+#        if packettype == 0:  # Heartbeat
+#            hbmaxschema = self.getuint(payload[0:4])
+#            hbversion_len = self.getint(payload[4:8])
+#            hbversion = payload[8 : 8 + hbversion_len].decode()
+#            print(
+#                f"heartbeat: sv:{version} p:{packettype} "
+#                f"u:{unique}: ms:{hbmaxschema} av:{hbversion}"
+#            )
+#            return
+#
+#        if packettype == 1:  # Status
+#            ...
+#            [dialfreq] = struct.unpack(">Q", payload[0:8])
+#            modelen = self.getint(payload[8:12])
+#            mode = payload[12 : 12 + modelen].decode()
+#            payload = payload[12 + modelen :]
+#            dxcalllen = self.getint(payload[0:4])
+#            dxcall = payload[4 : 4 + dxcalllen].decode()
+#            print(
+#                f"Status: sv:{version} p:{packettype} u:{unique} df:{dialfreq} m:{mode} dxc:{dxcall}"
+#            )
+#
+#            # if f"{dxcall}{self.band}{self.mode}" in self.dupdict:
+#            #     self.ft8dupe = f"{dxcall} {self.band}M {self.mode} FT8 Dupe!"
+#            return
+#
+#        if packettype == 2:  # Decode commented out because we really don't care
+#            return
+#
+#        if packettype != 12:
+#            return  # bail if not logged ADIF
+#        # if log packet it will contain this nugget.
         gotcall = datagram.find(b"<call:")
         if gotcall != -1:
             datagram = datagram[gotcall:]  # strip everything else
